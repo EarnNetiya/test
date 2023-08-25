483045f910fdbfe6e6762558dec456c765317156
EarnNetiya <s6506022630015@email.kmutnb.ac.th>
2023-08-25 09:48:11 +0700

    new

--- mid-1/66/capToFront.js
+++ mid-1/66/capToFront.js
@@ -1,19 +1,18 @@
 function capToFront(str){
     let lower = []
     let upper = []
-    let result = upper.concat(lower).join('')
     let arr = str.split('')
-    for (let i = 0; i < arr.legth; i++){
+    for (let i = 0; i < arr.length; i++){
         if(arr[i] === arr[i].toUpperCase()){
             upper.push(arr[i])
         }else{
             lower.push(arr[i])
         }
     }
-    return result
+    return upper.concat(lower).join('')
+
 }
 console.log(capToFront("hApPy"))
 console.log(capToFront("moveMENT"))
 console.log(capToFront("shOrtCAKE"))
 
-/// xxxxxxxxxxxxxxxxx
