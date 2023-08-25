67e9718d31f1dd0eeabafe76056cc1412ceabbaa
EarnNetiya <s6506022630015@email.kmutnb.ac.th>
2023-08-25 10:11:03 +0700

    new

--- mid-1/66/frequencySort.js
+++ mid-1/66/frequencySort.js
@@ -1,22 +1,5 @@
 function frequencySort(str){
-    let arr = str.split("")
-    let map = []
-    let result = ""
-    for(let i = 0; i < arr.length; i++){
-        if(map[arr[i]]){
-            map[arr[i]]++
-        }
-        else{
-            map[arr[i]] = 1
-        }
-    }
-    map.sort((a, b) => b - a)
-    for(let i = 0; i < map.length; i++){
-        for(let j = 0; j < map[i]; j++){
-            result += map[i]
-        }
-    }
-    return result
+   
 }
 
 
